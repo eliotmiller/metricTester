5cc1616050021e8d86ee8f7b42c0b93d1c7fa280
eliotmiller <eliotmiller@umsl.edu>
2013-09-30 15:33:26 -0500

    Adding 1s and 2s nulls
    Added 1s and 2s nulls from spacodiR to the singlemetric and allmetric
    nulls

--- R/allMetricsNull.R
+++ R/allMetricsNull.R
@@ -1,7 +1,7 @@
 #' Generate null expectations for community structure metrics
 #'
 #' Given a phylo object, a picante-style community data matrix (sites are rows,
-#' species are columns), a desired null method (any of picante or also 2x or 3x of
+#' species are columns), a desired null method (any of picante or also 2x, 3x, 1s, & 2s of
 #' spacodiR), a desired number of randomizations, and an output file name, will shuffle
 #' matrix according to null method, then calculate all phylogenetic community structure
 #' metrics as defined in the allMetrics() function, then save each iteration's worth of 
@@ -11,15 +11,15 @@
 #' @param tree Phylo object
 #' @param orig.matrix A picante-style community data matrix with sites as rows, and
 #' species as columns
-#' @param null.method A picante-style null, e.g. "richness" or "frequency", or either "2x"
-#' or "3x", which will call spacodiR
+#' @param null.method A picante-style null, e.g. "richness" or "frequency", or "2x", "3x"
+#' "1s" or "2s", which will call spacodiR
 #' @param no.randomizations The desired number of no.randomizations the function will run,
 #' i.e. the number of times orig.matrix will be shuffled and the metrics calculated on it
 #' @param temp.file The desired name of the output csv file
 #' 
 #' @details This runs much faster than trying to do this in memory in R. I will upload
 #' some of those type of functions in the near future anyhow. If you call null metrics
-#' 2x or 3x, it will call spacodiR for the resampling
+#' 2x, 3x, 1s or 2s, it will call spacodiR for the resampling
 #'
 #' @return A csv file with each column equal to the value of a given metric for the
 #' shuffled community in question (a row in the input matrix). 
@@ -58,6 +58,20 @@ allMetricsNull <- function(tree, orig.matrix, null.method, no.randomizations, te
 		{
 			spacodi.cdm <- suppressMessages(as.spacodi(orig.matrix))
 			temp.matrix <- resamp.3x(spacodi.cdm)
+			new.matrix <- suppressMessages(as.picante(temp.matrix))
+		}
+
+		else if(null.method=="1s")
+		{
+			spacodi.cdm <- suppressMessages(as.spacodi(orig.matrix))
+			temp.matrix <- resamp.1s(spacodi.cdm)
+			new.matrix <- suppressMessages(as.picante(temp.matrix))
+		}
+
+		else if(null.method=="2s")
+		{
+			spacodi.cdm <- suppressMessages(as.spacodi(orig.matrix))
+			temp.matrix <- resamp.2s(spacodi.cdm)
 			new.matrix <- suppressMessages(as.picante(temp.matrix))
 		}
 
