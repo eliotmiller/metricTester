40bffc219bd3329f6710f649aa58b2332f8cc1c2
Alexf4 <alexf4@gmail.com>
2015-02-08 17:50:44 -0800

    got the cron job working!

--- AWS/Starter.py
+++ AWS/Starter.py
@@ -1,6 +1,7 @@
 
 #!/usr/bin/python
 
+
 __author__ = 'alexf4'
 import sys
 import datetime
@@ -11,14 +12,10 @@ import os
 #This is the entry point into the python program
 def main():
 
-
+    sys.path.append("/usr/local/bin/aws")
     #check to see if temp file exists
     if os.path.exists("running") is False:
         tempfile = open("running", "wb")
-
-    #This will be an infinite loop
-    #while(True):
-
 
         #run command the command line. This would be the R commands you need to give out
         subprocess.call("echo Hello World", shell=True)
@@ -27,17 +24,16 @@ def main():
         name = datetime.datetime.now().isoformat() + ".txt"
         fo = open(name, "wb")
 
-        #Build the correct command string
-        command = "aws s3 mv " + name +" s3://alex-ec2-usage-data"
 
-        #New command
-        #command = "aws s3 mv . s3://alex-ec2-usage-data --exclude \"*\" --include \"*.txt\" "
+        #Build the correct command string
+        command = "/usr/local/bin/aws s3 mv " + name +" s3://alex-ec2-usage-data"
 
         #send file to S3 with AWS CLI
         subprocess.call(command , shell=True)
 
-    else:
-        return 0
+        #rm the running file so the cron job will start this script again.
+        subprocess.call("rm running" , shell=True)
+
 
 if __name__ == "__main__":
     main()
